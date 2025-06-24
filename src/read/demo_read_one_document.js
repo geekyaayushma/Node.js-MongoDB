843720ed2803eca5eccf867500c438ad1e59d489
geekyaayushma <mahataayushma653@gmail.com>
2025-06-24 14:06:43 +0545

    Read single document from database

---
+++ src/read/demo_read_one_document.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require("mongodb");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Function to find one listing by name
+async function findOneListingByName(client, nameOfListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .findOne({ name: nameOfListing });
+
+  if (result) {
+    console.log(`✅ Found a listing with the name '${nameOfListing}':`);
+    console.log(result);
+  } else {
+    console.log(`❌ No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+// Main function
+async function main() {
+  try {
+    await client.connect();
+    console.log("🔌 Connected to MongoDB");
+
+    // Search for the listing
+    await findOneListingByName(client, "Infinite Views");
+  } catch (err) {
+    console.error("❗ Error occurred:", err);
+  } finally {
+    await client.close();
+    console.log(" Connection closed");
+  }
+}
+
+main().catch(console.error);
